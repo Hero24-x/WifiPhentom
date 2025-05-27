9c37427eaf08e0fec9d6b0e0f7044b26d4fc58f3
Hero24-00 <196810131+Hero24-x@users.noreply.github.com>
2025-05-27 10:23:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from modules.scanner import scan_networks
 from modules.detector import detect_evil_twin
 from modules.auto_disconnect import disconnect_suspicious
 from telegram.notifier import send_alert
+
+if __name__ == "__main__":
+    print("\n[+] WiFiPhantom Starting...\n")
+    networks = scan_networks()
+    rogue = detect_evil_twin(networks)
+    if rogue:
+        send_alert(rogue)
+        disconnect_suspicious(rogue)
+      
